2055764244b3b3afb5b2c992b89d3d4283dfd4f7
Eduard Kazakov <silenteddie@gmail.com>
2017-08-10 15:32:55 +0300

    from urllib to requests; ogr source support

--- S1Downloader.py
+++ S1Downloader.py
@@ -5,14 +5,18 @@ Created on Wed Aug  9 12:49:33 2017
 @author: silent
 """
 
-import urllib2, urllib
+#import urllib2, urllib
 import os
 from datetime import datetime
 import xml.etree.ElementTree as etree
+import ogr
+import requests
+import sys
+from requests.auth import HTTPBasicAuth
 
 class S1Downloader():
     
-    opensearch_base_url =  'https://scihub.copernicus.eu/dhus/' 
+    opensearch_base_url =  'https://scihub.copernicus.eu/dhus/search' 
     odata_base_url = 'https://scihub.copernicus.eu/dhus/odata/v1/'
     
     sensor_modes= ['SW','IW','EW','WV']
@@ -23,31 +27,24 @@ class S1Downloader():
         self.username = username
         self.password = password
         self.download_dir = download_dir
-        
-        self.__connect()
-        
-        
-    def __connect(self):
-        self.password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
-        self.password_mgr.add_password(None, self.opensearch_base_url, self.username, self.password)
-        self.handler = urllib2.HTTPBasicAuthHandler(self.password_mgr)  
-        self.opener = urllib2.build_opener(self.handler)  
-        urllib2.install_opener(self.opener)
 
-    def search_by_conditions (self, wkt_region=None, start_date=None, end_date=None, sensor_modes=None, polarisations=None, product_types=None, extra=None):
+    def search_by_conditions (self, wkt_region=None, ogr_source=None, start_date=None, end_date=None, sensor_modes=None, polarisations=None, product_types=None, extra=None):
         rows = 100
-        
-        search_string = self.opensearch_base_url + 'search?q='
+        #search_string = self.opensearch_base_url + 'search?q='
         conditions = []
-        if wkt_region:
+        if wkt_region and not ogr_source:
             conditions.append('footprint:"Intersects(%s)"' % wkt_region)
+        if ogr_source:
+            conditions.append('footprint:"Intersects(%s)"' % self.__get_first_feature_wkt_from_ogr_source(ogr_source))
+            
         if start_date and not end_date:
             conditions.append('beginposition:[%s TO NOW]' % self.__datetime_to_scihub_format(start_date))
         if start_date and end_date:
             conditions.append('beginposition:[%s TO %s]' % (self.__datetime_to_scihub_format(start_date),self.__datetime_to_scihub_format(end_date)))
-            conditions.append('endposition:[%s TO %s]' % (self.__datetime_to_scihub_format(start_date),self.__datetime_to_scihub_format(end_date)))
+            #conditions.append('endposition:[%s TO %s]' % (self.__datetime_to_scihub_format(start_date),self.__datetime_to_scihub_format(end_date)))
         if end_date and not start_date:
             conditions.append('endposition:[%s TO %s]' % (self.__datetime_to_scihub_format(datetime(year=1970,month=1,day=1)),self.__datetime_to_scihub_format(end_date)))
+        
         if sensor_modes:
             sensor_conditions = []
             for sensor_mode in sensor_modes:
@@ -91,7 +88,7 @@ class S1Downloader():
                 conditions.append('(' + ' OR '.join(product_conditions) + ')')
         
         # service
-        conditions.append('S1')
+        conditions.append('(SAR OR Sentinel-1)')
         
         
         if conditions:
@@ -100,20 +97,16 @@ class S1Downloader():
             print 'No conditions specified!'
             return []
         
-        base_search_string = search_string + conditions
-        print base_search_string
-        base_search_string = base_search_string + '&rows=%s' % str(rows)
-
         
+        datasource = requests.get(self.opensearch_base_url,params={'q': conditions, 'rows':str(rows), 'orderby':'beginposition asc'}, auth=HTTPBasicAuth(self.username,self.password))
         # First step: get number of results:
-        datasource = urllib.urlopen(base_search_string)
-        number_of_results = int(etree.parse(datasource).getroot().find('{http://a9.com/-/spec/opensearch/1.1/}totalResults').text)
+        number_of_results = int(etree.ElementTree(etree.fromstring(datasource.content)).getroot().find('{http://a9.com/-/spec/opensearch/1.1/}totalResults').text)
         print number_of_results, ' results'
         
         searched_data = []
-            
+
         if number_of_results <= rows:
-            answer_data = etree.parse(datasource).getroot()
+            answer_data = etree.ElementTree(etree.fromstring(datasource.content)).getroot()
             entries = answer_data.findall('{http://www.w3.org/2005/Atom}entry')
             for entry in entries:
                 entry_properties = self.__get_entry_properties(entry) 
@@ -121,10 +114,9 @@ class S1Downloader():
             
         else:
             for start_row in range(0,number_of_results,rows):
-                current_search_string = base_search_string + '&start=%s' % str(start_row)
-                print current_search_string
-                datasource = urllib.urlopen(current_search_string)
-                answer_data = etree.parse(datasource).getroot()
+                print 'requesting results from %s to %s' % (str(start_row),str(start_row+int(rows)))
+                datasource = requests.get(self.opensearch_base_url,params={'q': conditions, 'start':str(start_row), 'rows':str(rows), 'orderby':'beginposition asc'}, auth=HTTPBasicAuth(self.username,self.password))
+                answer_data = etree.ElementTree(etree.fromstring(datasource.content)).getroot()
                 entries = answer_data.findall('{http://www.w3.org/2005/Atom}entry')
                 for entry in entries:
                     entry_properties = self.__get_entry_properties(entry) 
@@ -132,8 +124,9 @@ class S1Downloader():
                 
         return searched_data
         
-    def download_by_conditions (self, wkt_region=None, start_date=None, end_date=None, sensor_modes=None, polarisations=None, product_types=None, extra=None):
+    def download_by_conditions (self, wkt_region=None, ogr_source=None, start_date=None, end_date=None, sensor_modes=None, polarisations=None, product_types=None, extra=None, skip_rate=1):
         search_results = self.search_by_conditions(wkt_region=wkt_region,
+                                                   ogr_source=ogr_source,
                                                    start_date=start_date,
                                                    end_date=end_date,
                                                    sensor_modes=sensor_modes,
@@ -141,10 +134,15 @@ class S1Downloader():
                                                    product_types=product_types,
                                                    extra=extra)
         
-        for search_result in search_results:
+        for search_result in search_results[::skip_rate]:
             print 'Downloading %s (size:%s)' % (search_result['name'], search_result['size'])
-            current_path = self.download_dir + search_result['name'] + '.' + search_result['data_format']
+            current_path = self.download_dir + search_result['name'] + '.' + search_result['data_format']+ '.' + 'zip'
+            if os.path.exists(current_path):
+                print 'Dataset already exists'
+                continue
+            
             self.download_scene_by_id (search_result['uid'], current_path)
+            print 'Done'
                 
 
     def download_by_meta4_list (self, meta4_list):
@@ -161,15 +159,34 @@ class S1Downloader():
                     continue  
       
             print "Downloading ", fileName 
-            urllib.urlretrieve(sentinel_link, destinationpath)    
- 
+            
+            r = requests.get(sentinel_link, auth=HTTPBasicAuth(self.username,self.password), stream=True)
+        
+            current_size = 0
+            chunk_size = 4096
+            with open(destinationpath, 'wb') as fd:
+                for chunk in r.iter_content(chunk_size=chunk_size):
+                    sys.stdout.write('Megabytes downloaded: %s\r' % str(current_size/1024/1024.0))
+                    sys.stdout.flush()
+                    fd.write(chunk)
+                    current_size += chunk_size
+                    
             print "Done downloading"
         
         
     def download_scene_by_id (self, uid, downloading_path):
-        downloader_string = self.odata_base_url + 'Products(\'$s\')/$value' % str(uid)
-        print 'Downloading %s' % str(id)
-        urllib.urlretrieve(downloader_string,downloading_path)
+        downloader_string = self.odata_base_url + 'Products(\'%s\')/$value' % str(uid)
+        r = requests.get(downloader_string, auth=HTTPBasicAuth(self.username,self.password), stream=True)
+        
+        current_size = 0
+        chunk_size = 4096
+        with open(downloading_path, 'wb') as fd:
+            for chunk in r.iter_content(chunk_size=chunk_size):
+                sys.stdout.write('Megabytes downloaded: %s\r' % str(current_size/1024/1024.0))
+                sys.stdout.flush()
+                fd.write(chunk)
+                current_size += chunk_size
+        print ''
         
     def __datetime_to_scihub_format(self, user_datetime):
         return user_datetime.strftime("%Y-%m-%dT%H:%M:%SZ")
@@ -182,6 +199,7 @@ class S1Downloader():
         size = ''
         uuid = ''
         name = ''
+        data_format = ''
         
         for child in entry:
             if child.get('name') == 'producttype':
@@ -198,7 +216,14 @@ class S1Downloader():
                 uuid = child.text
             if child.get('name') == 'identifier':
                 name = child.text
-            if child.get('name') == 'data_format':
+            if child.get('name') == 'format':
                 data_format = child.text
         return {'product_type':product_type, 'polarisation':polarisation, 'start_date':start_date,
                 'end_date':end_date, 'size':size, 'uid':uuid, 'name':name, 'data_format':data_format}
+        
+    def __get_first_feature_wkt_from_ogr_source (self, ogr_source_path):
+        ogr_source = ogr.Open(ogr_source_path)
+        layer = ogr_source.GetLayer(0)
+        feature = layer.GetFeature(0)
+        geometry = feature.GetGeometryRef()
+        return geometry.ExportToWkt()
